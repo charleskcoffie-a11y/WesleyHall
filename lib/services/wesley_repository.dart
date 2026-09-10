cebb30dbac4121db90b1196fe5f314acdc69209b
Kingscoph <charleskcoffie@gmail.com>
2026-09-10 13:49:25 -0400

    Expose booking audit timeline in repository

--- lib/services/wesley_repository.dart
+++ lib/services/wesley_repository.dart
@@ -23,6 +23,7 @@ abstract class WesleyRepository {
   Future<Uint8List?> loadClientSignature(String? path);
 
   Future<List<Booking>> listBookings();
+  Future<List<AuditEvent>> listAuditLog(String bookingId);
   Future<void> createBooking(Booking booking);
   Future<void> updateBooking(Booking booking);
   Future<void> addPayment(String bookingId, PaymentRecord payment);
